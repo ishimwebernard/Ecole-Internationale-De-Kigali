405b1fdcc13b1ad52d757e08a08f07a9e0c6a8b2
“ishimwebernard” <isbernard2001@gmail.com>
2020-12-14 10:18:22 +0200

    Finalising the landing page

--- UI/js/index.js
+++ UI/js/index.js
@@ -4,6 +4,7 @@ const schoolLogo = document.getElementById("schoollogo");
 var navgroup = document.getElementById('navgroup');
 var slide = document.getElementById('slide');
 var row2 = document.getElementById('row2');
+var slideIndex = 1;
 
 
 const tl = new TimelineMax();
@@ -11,6 +12,7 @@ const tl = new TimelineMax();
 tl.fromTo(schoolLogo, 1.5, {x: "-100%"},{x:"0%", ease: Power2.easeInOut})
 .fromTo(navgroup, 1, {opacity: 0}, {opacity: 1},"-=.5")
 .fromTo(slide,2.5,{height:0},{height:"31rem",ease: Power2.easeInOut},"-=1.5");
+showSlides();
 
 //Row 2 Animation
 
@@ -40,3 +42,22 @@ window.addEventListener("scroll", function() {
 
     
   });
+
+  function showSlides() {
+    const _SLIDEANIMATOR = new TimelineMax();
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    var target = slides[slideIndex-1];
+    var targetParagraph = target.childNodes[3];
+    slides[slideIndex-1].style.display = "block";
+    _SLIDEANIMATOR.fromTo(target,1,{opacity: .7}, {opacity: 1})
+    .fromTo(targetParagraph, 1, {y:"100%"},{y: "0%",ease: Power2.easeInOut},"-=1");
+    
+    
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
+  }
