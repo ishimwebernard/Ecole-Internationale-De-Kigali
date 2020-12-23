bc64edea328671cc93b7e57d6469b689dfde2478
“ishimwebernard” <isbernard2001@gmail.com>
2020-12-23 17:18:08 +0200

    Adding Video Portal

---
+++ UI/js/videoportal.js
@@ -0,0 +1,13 @@
+// var _VIDEO = document.querySelector("#video-element"),
+//     _CANVAS = document.querySelector("#canvas-element");
+
+// // Video metadata is loaded
+// _VIDEO.addEventListener('loadedmetadata', function() {
+//     // Set canvas dimensions same as video dimensions
+//     _CANVAS.width = _VIDEO.videoWidth;
+//     _CANVAS.height = _VIDEO.videoHeight;
+//     document.querySelector("#video-element").addEventListener('timeupdate', function() {
+//         console.log(_CANVAS.toDataURL());
+//     });
+// });
+
